630f2d1c7fa13517add52c66666bf4a50e8ae71f
Samplasion <ercolino2004@gmail.com>
2018-09-19 15:28:12 +0200

    Add jsdoc to isSingular()

--- src/CheckUtils.js
+++ src/CheckUtils.js
@@ -7,6 +7,8 @@ class CheckUtils {
 
     /**
      * Checks if the given noun is singular
+     @param {string} noun The noun
+     @return boolean <code>true</code> if the noun is singular, <code>false</code> otherwise
      */
     static isSingular(noun) {
 	return wordSingular(noun) == noun;
