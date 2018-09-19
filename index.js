7d68e32cfa0900a317ee71b37c3cfaeacf94656f
Samplasion <ercolino2004@gmail.com>
2018-09-19 16:06:45 +0200

    Update index.js
    Version 0.5.0

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { plural, wordPlural, wordSingular } = require('./src/index.js');
 const CheckUtils = require('./src/CheckUtils.js');
+const Noun = require('./src/Noun.js');
 
 module.exports = {
     plural,
     wordPlural,
     wordSingular,
-    CheckUtils
+    CheckUtils,
+    Noun
 }
